4ef2bd5ef7fbbfb37893a900b9f69cdec64ea200
meSajied <sajiedshahyousuf@gmail.com>
2023-11-08 18:16:50 +0600

    Initial commit

---
+++ logger.js
@@ -0,0 +1,16 @@
+const log4js = require('log4js');
+
+log4js.configure({
+  appenders: {
+    console: {type: 'console', layout: {type: 'coloured'}
+    }
+  },
+  categories: {
+    default: {
+      appenders: ['console'], level: 'all'
+    }
+  }
+});
+
+const logger = log4js.getLogger();
+module.exports = logger;
